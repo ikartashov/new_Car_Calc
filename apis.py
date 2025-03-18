e6a195366f98cfefdef7b05f78dd6b4d9a8a2b34
Игорь Карташов <ikartashov@MacBook-Pro-Bogdana.local>
2025-03-18 13:53:34 +0300

    Create project cr calculator

---
+++ apis.py
@@ -0,0 +1,8 @@
+def get_gas_price():
+    # TODO: Get price from API
+    return 8
+
+
+def get_power_price():
+    # TODO: Get price from API
+    return 1.2
---
+++ main.py
@@ -0,0 +1,14 @@
+import calculator
+
+if __name__ == '__main__':
+    calc = calculator.Calculator()
+    calc.add_car(
+        calculator.Car("Toyota Corolla", 110000,
+                       7,1200, 2500))
+    calc.add_car(
+        calculator.ElectricCar("Tesla Model 3", 200000,
+                               5500, 150))
+    calc.add_car(
+        calculator.Car("Range Rover", 650000,
+                       3, 3000, 7000))
+    calc.print_cars()
